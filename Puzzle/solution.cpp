d60cf324548467b96f8d939baf4e966436d63e80
NelsonGomesNeto <ngn@ic.ufal.br>
2018-07-15 14:34:35 -0300

    Implemented A* on C++, it's like a bullet, could even do a 6x6

--- Puzzle/solution.cpp
+++ Puzzle/solution.cpp
@@ -2,13 +2,21 @@
 #include <sys/resource.h>
 #define DEBUG if(0)
 #define lli long long int
+#define vv vector<vector<int> >
+#define ele pair<vv, pair<int, double> >
 using namespace std;
-int n, sqn; // table[1000][1000]
-vector<vector<int> > table;
-set<vector<vector<int> > > visitedSet;
-deque<pair<vector<vector<int> >, pair<int, pair<int, int> > > > q;
+int n, sqn;
+vv table;
+set<string> visitedSet;
+deque<pair<vv, int> > q;
+bool compare(ele a, ele b)
+{
+  return(a.second.second > b.second.second);
+}
+priority_queue<ele, vector<ele >, function<bool(ele, ele)> > pq(compare);
 int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-int minSteps = -1;
+int minSteps = -1, scrambleSteps = 1000;
+
 
 void printTable()
 {
@@ -30,20 +38,23 @@ int valid(int i, int j)
   return(!(i < 0 || i >= n || j < 0 || j >= n));
 }
 
-// string state()
-// {
-//   string nowState = "";
-//   for (int i = 0; i < n; i ++)
-//     for (int j = 0; j < n; j ++)
-//       nowState += ('0' + table[i][j]) + "|";
-//   return(nowState);
-// }
+string state()
+{
+  string aux = "";
+  for (int i = 0; i < n; i ++)
+    for (int j = 0; j < n; j ++)
+    {
+      aux.push_back('0' + table[i][j]);
+      if (i != n - 1 || j != n - 1) aux += "|";
+    }
+  return(aux);
+}
 
 void scramble()
 {
   for (int i = 0, k = 1; i < n; i ++) for (int j = 0; j < n; j ++, k ++) table[i][j] = k; table[n - 1][n - 1] = 0;
 
-  int s = 100, i = n - 1, j = n - 1;
+  int s = scrambleSteps, i = n - 1, j = n - 1;
   while (s --)
   {
     int dir; do dir = rand() % 4; while (!valid(i + dy[dir], j + dx[dir]));
@@ -63,7 +74,7 @@ int solved()
   return(1);
 }
 
-int dfs(int i, int j, int now)
+int dfsRecursive(int i, int j, int now)
 {
   // DEBUG { printf("hash: %s\n", state()); printTable(); }
   if (i == n - 1 && j == n - 1 && solved())
@@ -71,25 +82,29 @@ int dfs(int i, int j, int now)
     minSteps = now;
     return(1);
   }
-  if (visitedSet.count(table)) return(0);
-  visitedSet.insert(table);
 
   for (int k = 0; k < 4; k ++)
     if (valid(i + dy[k], j + dx[k]))
     {
       swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
-      if (dfs(i + dy[k], j + dx[k], now + 1)) return(1);
+      string nowState = state();
+      if (!visitedSet.count(nowState))
+      {
+        visitedSet.insert(nowState);
+        if (dfsRecursive(i + dy[k], j + dx[k], now + 1)) return(1);
+      }
       swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
     }
   return(0);
 }
 
-void bfs(int i, int j)
+void dfs(int i, int j)
 {
-  q.push_back({table, {0, {i, j}}});
+  q.push_front({table, 0});
   while (!q.empty())
   {
-    table = q.front().first; minSteps = q.front().second.first; i = q.front().second.second.first; j = q.front().second.second.second; q.pop_front();
+    table = q.front().first; minSteps = q.front().second; q.pop_front();
+    for (int l = 0; l < n; l ++) for (int m = 0; m < n; m ++) if (table[l][m] == 0) i = l, j = m;
     // printTable(); printf("\n");
     if (solved()) break;
 
@@ -97,16 +112,90 @@ void bfs(int i, int j)
       if (valid(i + dy[k], j + dx[k]))
       {
         swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
-        if (!visitedSet.count(table))
+        string nowState = state();
+        if (!visitedSet.count(nowState))
         {
-          visitedSet.insert(table);
-          q.push_back({table, {minSteps + 1, {i + dy[k], j + dx[k]}}});
+          visitedSet.insert(nowState);
+          q.push_front({table, minSteps + 1});
         }
         swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
       }
   }
-  // printTable();
   while (!q.empty()) q.pop_front();
+}
+
+void bfs(int i, int j)
+{
+  q.push_back({table, 0});
+  while (!q.empty())
+  {
+    table = q.front().first; minSteps = q.front().second; q.pop_front();
+    for (int l = 0; l < n; l ++) for (int m = 0; m < n; m ++) if (table[l][m] == 0) i = l, j = m;
+    if (solved()) break;
+
+    for (int k = 0; k < 4; k ++)
+      if (valid(i + dy[k], j + dx[k]))
+      {
+        swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
+        string nowState = state();
+        if (!visitedSet.count(nowState))
+        {
+          visitedSet.insert(nowState);
+          q.push_back({table, minSteps + 1});
+        }
+        swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
+      }
+  }
+  while (!q.empty()) q.pop_front();
+}
+
+double manhattanDistance()
+{
+  int dist = 0;
+  for (int i = 0; i < n; i ++)
+    for (int j = 0; j < n; j ++)
+    {
+      int at = table[i][j] - 1;
+      dist += abs(at / n - i) + abs(at % n - j);
+    }
+  return(dist);
+}
+
+double euclideanDistance()
+{
+  double dist = 0;
+  for (int i = 0; i < n; i ++)
+    for (int j = 0; j < n; j ++)
+    {
+      int at = table[i][j] - 1;
+      dist += sqrt(pow(at / n - i, 2) + pow(at % n - j, 2)); //sqrt
+    }
+  return(dist);
+}
+
+void astar(int i, int j, double (*heuristic)())
+{
+  pq.push({table, {0, heuristic()}});
+  while (!pq.empty())
+  {
+    table = pq.top().first; minSteps = pq.top().second.first; pq.pop();
+    for (int l = 0; l < n; l ++) for (int m = 0; m < n; m ++) if (table[l][m] == 0) i = l, j = m;
+    if (solved()) break;
+
+    for (int k = 0; k < 4; k ++)
+      if (valid(i + dy[k], j + dx[k]))
+      {
+        swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
+        string nowState = state();
+        if (!visitedSet.count(nowState))
+        {
+          visitedSet.insert(nowState);
+          pq.push({table, {minSteps + 1, heuristic()}});
+        }
+        swap(&table[i][j], &table[i + dy[k]][j + dx[k]]);
+      }
+  }
+  while (!pq.empty()) pq.pop();
 }
 
 int main()
@@ -119,25 +208,38 @@ int main()
 
   scanf("%d", &n); sqn = 9; int kk = 1;
   for (int i = 0; i < n; i ++) table.push_back(vector<int>(n));
-  while (1)
+  while (kk)
   {
     scramble();
-    vector<vector<int> > aux = table;
+    printTable();
+    vv aux = table;
     int si, sj;
     for (int i = 0; i < n; i ++) for (int j = 0; j < n; j ++) if (table[i][j] == 0) si = i, sj = j;
 
-    printTable();
-    printf("DFS:\n");
-    minSteps = -1;
-    visitedSet.clear();
-    dfs(si, sj, 0);
-    printf("\tReached %ld different states\n", visitedSet.size());
-    printf("\tTook %d steps\n", minSteps);
+    // printf("DFS:\n");
+    // visitedSet.clear();
+    // dfs(si, sj);
+    // printf("\tReached %ld different states\n", visitedSet.size());
+    // printf("\tTook %d steps\n", minSteps);
+    //
+    // table = aux;
+    // printf("BFS:\n");
+    // visitedSet.clear();
+    // bfs(si, sj);
+    // printf("\tReached %ld different states\n", visitedSet.size());
+    // printf("\tTook %d steps\n\n", minSteps);
+
+    // table = aux;
+    // printf("A* (manhattanDistance):\n");
+    // visitedSet.clear();
+    // astar(si, sj, manhattanDistance);
+    // printf("\tReached %ld different states\n", visitedSet.size());
+    // printf("\tTook %d steps\n\n", minSteps);
 
     table = aux;
-    printf("BFS:\n");
+    printf("A* (euclideanDistance):\n");
     visitedSet.clear();
-    bfs(si, sj);
+    astar(si, sj, euclideanDistance);
     printf("\tReached %ld different states\n", visitedSet.size());
     printf("\tTook %d steps\n\n", minSteps);
   }
